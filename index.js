57a74709164662e9ee653a484433df2140c1ad33
MilonMridha <milonmridha570@gmail.com>
2022-02-27 23:01:16 +0600

    something correction

--- index.js
+++ index.js
@@ -1,73 +1,71 @@
 const loadCocktails = () => {
-    const inputField = document.getElementById('input-field');
-    const inputFieldText = inputField.value;
+  const inputField = document.getElementById("input-field");
+  const inputFieldText = inputField.value;
+  const drinkDiv = document.getElementById("drink");
+  drinkDiv.innerHTML = "";
+  inputField.value = "";
+  if (inputFieldText == "" || !isNaN(inputFieldText)) {
+    alert("Enter String Name");
+  }
+  // LoadCocktails Data ---------------->
+  else {
+    toggleSpinner("block");
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputFieldText}`;
 
-    inputField.value = '';
-    if(inputFieldText == '' || !isNaN(inputFieldText)){
-        alert('Enter String Name')
-    }
-    // LoadCocktails Data ---------------->
-    else{
-        toggleSpinner('block');
-        const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputFieldText}`
-    
-        fetch(url)
-        .then(res => res.json())
-        .then(data => displayCocktails(data.drinks))
-    }
-}
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => displayCocktails(data.drinks));
+  }
+};
 // toggle spinner function-------------->
-const toggleSpinner = displayStyle => {
-    document.getElementById('spinner').style.display = displayStyle;
-}
-
+const toggleSpinner = (displayStyle) => {
+  document.getElementById("spinner").style.display = displayStyle;
+};
 
 //Display cocktail data in UI---------------->
 
-const displayCocktails = cocktails => {
-    const main = document.getElementById('main');
-    main.innerHTML = '';
-    if(!cocktails){
-        alert('result not found')
-        toggleSpinner('none')
-    }
-    else{
-        cocktails.forEach(cocktail => {
-            const div = document.createElement('div');
-                div.classList.add('col-lg-4');
-                div.classList.add('mt-3');
-                div.innerHTML = `
+const displayCocktails = (cocktails) => {
+  const main = document.getElementById("main");
+  main.innerHTML = "";
+  if (!cocktails) {
+    alert("result not found");
+    toggleSpinner("none");
+  } else {
+    cocktails.forEach((cocktail) => {
+      const div = document.createElement("div");
+      div.classList.add("col-lg-4");
+      div.classList.add("mt-3");
+      div.innerHTML = `
                 <div class="card" style="width: 18rem;">
           <img src="${cocktail.strDrinkThumb} " class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">${cocktail.strDrink} </h5>
             <p class="card-text">${cocktail.strInstructions?.slice(0, 80)} </p>
-            <a href="#" onclick="loadSeeDetails('${cocktail.idDrink}')" class="btn btn-primary">See Details</a>
+            <a href="#" onclick="loadSeeDetails('${
+              cocktail.idDrink
+            }')" class="btn btn-primary">See Details</a>
           </div>
         </div>
-                `
-                main.appendChild(div);
-                toggleSpinner('none');
-            });
-    }
-    
-
-}
+                `;
+      main.appendChild(div);
+      toggleSpinner("none");
+    });
+  }
+};
 // Load See Details------------->
-const loadSeeDetails = drinkId => {
-    const url = (`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`)
-    toggleSpinner('block');
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displayDetails(data.drinks[0]))
-}
+const loadSeeDetails = (drinkId) => {
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`;
+  toggleSpinner("block");
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayDetails(data.drinks[0]));
+};
 // Display see details Div--------------->
-const displayDetails = drink => {
-   
-    const drinkDiv = document.getElementById('drink');
-    drinkDiv.innerHTML = '';
-    const div = document.createElement('div');
-    div.innerHTML = `
+const displayDetails = (drink) => {
+  const drinkDiv = document.getElementById("drink");
+  drinkDiv.innerHTML = "";
+  const div = document.createElement("div");
+  div.innerHTML = `
     <div class="card" style="width: 18rem;">
       <img src="${drink.strDrinkThumb} " class="card-img-top" alt="...">
       <div class="card-body">
@@ -78,8 +76,7 @@ const displayDetails = drink => {
         <p class="card-text">${drink.strInstructions}</p>
         </div>
     </div>
-    `
-    drinkDiv.appendChild(div);
-    toggleSpinner('none');
-}
-
+    `;
+  drinkDiv.appendChild(div);
+  toggleSpinner("none");
+};
